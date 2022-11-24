79f8d6b127cf0456b26643b2518cdc8391e13f6f
MintBrain <maximka602@gmail.com>
2022-11-25 00:38:30 +0500

    Изменение строки в develop

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import report
 import table
 
-# Пример изменения строки 2.2.3
+# Пример изменения строки 2.2.3 в develop
 if __name__ == '__main__':
     if input('Введите данные для печати: ') == 'Статистика':
         report.main()
